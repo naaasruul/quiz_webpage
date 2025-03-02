e6b495c7a91aafa3641e3d241857db8f09e594dd
Nasrulhaq Hidayat Abdul Latiff <nasrulhaqhidayat@gmail.com>
2025-03-03 02:00:43 +0800

    setup route

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,10 +1,7 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
-
-const Home = () => <h2>Home Page</h2>;
-const About = () => <h2>About Page</h2>;
-const Contact = () => <h2>Contact Page</h2>;
+import Home from './pages/Home';
 
 function App() {
   const handleAnimationComplete = () => {
@@ -13,20 +10,12 @@ function App() {
 
   return (
     <>
-     <Router>
-     <nav>
-        <Link to="/">Home</Link> | 
-        <Link to="/about">About</Link> | 
-        <Link to="/contact">Contact</Link>
-      </nav>
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-     </Router>
-    
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* add another page later.. */}
+        </Routes>
+      </Router>
     </>
   );
 }
---
+++ react/src/pages/Home.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const Home = () => <h2>Home Page</h2>;
+
+export default Home;
+
